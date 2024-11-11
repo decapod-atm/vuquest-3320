68f45f8f81401b9d4b9d1c0cd6367a0930778de8
cr8t <124842229+cr8t@users.noreply.github.com>
2024-11-11 21:05:24 +0000

    command: add macro helper to define command
    Adds a macro helper to define modifier commands.

--- src/command/macros.rs
+++ src/command/macros.rs
@@ -1,3 +1,65 @@
+#[macro_export]
+macro_rules! modifier_command {
+    (
+        $(#[$doc:meta])+
+        $cmd:ident: $prefix:literal { $($field:ident: $field_ty:ident$(,)?)+ }$(,)?) => {
+        paste::paste! {
+            $(#[$doc])+
+            #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+            pub struct $cmd {
+                $($field: Option<$field_ty>,)+
+            }
+
+            impl $cmd {
+                #[doc = "Creates a new [" $cmd "]."]
+                pub const fn new() -> Self {
+                    Self {
+                        $($field: None,)+
+                    }
+                }
+
+                #[doc = "Gets the ASCII serial command code for [" $cmd "]."]
+                pub fn command(&self) -> alloc::string::String {
+                    format!("{self}")
+                }
+
+                #[doc = "Gets the command ASCII prefix for [" $cmd "]."]
+                pub const fn prefix() -> &'static str {
+                    $prefix
+                }
+            }
+
+            impl Default for $cmd {
+                fn default() -> Self {
+                    Self::new()
+                }
+            }
+
+            impl TryFrom<&str> for $cmd {
+                type Error = $crate::result::Error;
+
+                fn try_from(val: &str) -> $crate::result::Result<Self> {
+                    let rem = val.strip_prefix($prefix).ok_or($crate::result::Error::InvalidVariant)?;
+
+                    Ok(Self {
+                        $($field: $field_ty::try_from(rem).ok(),)+
+                    })
+                }
+            }
+
+            impl core::fmt::Display for $cmd {
+                fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result {
+                    write!(f, "{}", $prefix)?;
+                    $(
+                        write!(f, "{}", self.$field.map(|c| alloc::string::String::from(c.command())).unwrap_or_default())?;
+                    )+
+                    Ok(())
+                }
+            }
+        }
+    }
+}
+
 /// Helper macro to define command modifier field access.
 #[macro_export]
 macro_rules! modifier_field {
